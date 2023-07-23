660b093f12f04fab6816456d04bc398d234983a9
AhmedRabiea <a.rabie.eissa@gmail.com>
2023-07-24 00:10:55 +0300

    overflow form scroll

--- src/components/myModal/MyModal.jsx
+++ src/components/myModal/MyModal.jsx
@@ -8,7 +8,7 @@ const MyModal = ({ visible, onClose, onCancel }) => {
     <div
       onClick={onClose}
       id="container"
-      className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center"
+      className="fixed  inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center overflow-auto"
     >
       <div className="bg-white p-1 mx-auto md:mx-0 w-full md:w-auto md:p-10  border-2 rounded-lg">
         <PopupRegisteration onClose={onClose} onCancel={onCancel} />
